43ee98334f153c143d742158b6ba3ff0868c62cf
archiebenn <archiesnbenn@gmail.com>
2026-06-03 18:00:45 +0100

    first commit

---
+++ jun_26/hello.c
@@ -0,0 +1,17 @@
+/* my first C program 3-6-2026 */
+
+/*
+include standard input/output functions in this common 'header':
+*/
+#include <stdio.h>
+
+/*
+declare the obligatory main() function
+*/
+int main() 
+{
+    printf("Hello World\n");
+
+    /* return 0 to confirm the program ran ok*/ 
+    return 0;
+}
