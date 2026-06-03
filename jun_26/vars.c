3b99fcfa894ae6911e1dc432083b1a1220b07e7d
archiebenn <archiesnbenn@gmail.com>
2026-06-03 21:14:12 +0100

    variables

---
+++ jun_26/vars.c
@@ -0,0 +1,26 @@
+/* script to learn about variables in C */
+
+/* different to R/python as you first declare the variable data type and the name, then the value later 
+
+these declarations should be made before any executable code appears in a program */
+
+#include <stdio.h>
+
+int main() {
+    /* declare variables */
+    int num1, num2;
+    char letter;
+    float decimal;
+
+    /* initialise variables */
+    num1 = 100;
+    num2 = 14;
+    letter = 'A';
+    decimal = 7.42; 
+
+    /* now something to print using double data type (up to 10 d.p) 
+    nb. %f sets it to 6dp */
+    double pi = 3.1415926536;
+    printf("The value of pi is approximately %f", pi );
+    return 0;
+}
